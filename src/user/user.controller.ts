3cfde7be48792c41b2333fae521421a29fda0225
Georjane <pzgeorjane@gmail.com>
2025-10-27 17:21:46 +0100

    Add route to get all users from prisma

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,9 +3,16 @@ import type { Request } from 'express';
 import type { User } from 'generated/prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+    constructor(private readonly userService: UserService) {}
+        @Get()
+        async findAll(): Promise<User[]> {
+            return this.userService.findAll();
+        }
+
     @UseGuards(JwtGuard)
     @Get('me')
     getMe(@GetUser() user: User) {
---
+++ src/user/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'generated/prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async findAll(): Promise<User[]> {
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        hash: true,
+        firstName: true,
+        lastsName: true,
+        createdAt: true,
+        updatedAt: true,
+        // Exclude the hash field for security reasons
+      },
+    });
+  }
+}
