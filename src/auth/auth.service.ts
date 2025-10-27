505ec4232b3b150b353d793dc9c9fc87674af8e6
Georjane <pzgeorjane@gmail.com>
2025-10-27 13:48:17 +0100

    Signup and Signin logic added and working

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,65 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2"
+import { PrismaClientKnownRequestError } from "generated/prisma/internal/prismaNamespace";
 
-@Injectable({})
+@Injectable()
 
 export class AuthService{
     constructor(private prisma: PrismaService) {}
-    signup() {
-        return { msg: 'sign up success'}
+    async signup(dto: AuthDto) {
+        // generate d password hash
+const hash = await argon.hash(dto.password)
+        // save the new user in the db
+
+        try {
+
+const user = await this.prisma.user.create({
+    data: {
+        email: dto.email,
+        hash
     }
 
-    signin() {
-        return 'sign in working'
+    })
+    const { hash: _, ...userWithoutHash } = user; // ✅ exclude hash safely
+  return userWithoutHash;}
+  catch(error){
+    if (error instanceof PrismaClientKnownRequestError) {
+        if(error.code === 'P2002'){
+            throw new ForbiddenException('Credentials taken')
+        }
+    }
+    throw error
+  }
+    }
+
+    async signin(dto: AuthDto) {
+        // find user by email
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        })
+
+        // if user does not exist throw exception
+
+        if(!user)
+            throw new ForbiddenException(
+        'Credentials Incorrect')
+        // compare password
+
+        const pwMatches = await argon.verify(
+            user.hash,
+            dto.password
+        )
+        // if password incorrect throw exception
+
+        if (!pwMatches)
+            throw new ForbiddenException(
+        'Credentials Incorrect')
+        // else send back user
+        const { hash: _, ...userWithoutHash } = user; // ✅ exclude hash safely
+  return userWithoutHash;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+      whitelist: true, // strips unknown properties
+      // forbidNonWhitelisted: true, // throws error if unknown property is present
+      transform: true, // transforms payloads into DTO instances
+}))
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
